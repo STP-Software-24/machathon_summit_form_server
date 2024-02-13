1d133ba775e776d21ceeed16640c18ddf430eb19
Apolo151 <abdallah.ibrahim5115@gmail.com>
2024-02-13 07:43:21 +0200

    added endpoint to check if user exists

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express,{ RequestHandler, ErrorRequestHandler } from 'express';
 import cors from 'cors';
-//import { dbPool } from './database';
 
 const bodyParser = require('body-parser')
 const { body, validationResult } = require('express-validator');
@@ -9,20 +8,17 @@ require('dotenv').config();
 
 import { Pool } from 'pg';
 
+// TODO: Change to prod DB
 export const dbPool = new Pool({
-    connectionString: process.env.PROD_DB_CONNECTION_STRING
+    connectionString: process.env.TEST_DB_CONNECTION_STRING
 });
 
 const app = express();
 
-//console.log(process.env.PROD_DB_CONNECTION_STRING)
 app.use(cors());
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-
-const posts: any[] = [];
 
 // insert attendee into database
 app.post('/register',
@@ -40,7 +36,7 @@ body("faculty").isString(),
 (req, res) => {
     const errors = validationResult(req);
     // validate data
-    if (!errors.isEmpty()||(req.body['grad_year']>2030|| req.body['grad_year']<1990)) {
+    if (!errors.isEmpty()||(req.body['grad_year']>2033|| req.body['grad_year']<1990)) {
         return res.status(400).json({
             success: false,
             errors: errors.array()
@@ -49,27 +45,38 @@ body("faculty").isString(),
     // insert into database
     const insertQuery = 'INSERT INTO stp.machathon_summit VALUES ($1, $2, $3, $4, $5, $6, $7, NOW());';
     const {name, phone_number, email, national_id, university, faculty, grad_year} = req.body;
-    posts.push(req.body);
     //
     dbPool.query(insertQuery, [name, phone_number, email, national_id, university, faculty, grad_year], (error, results) => {
         if(error){
+            res.status(500).json({
+                success: false,
+                message: "internal error, try again later" //error.message
+            })
             throw error;
         }
-    })
-    //
-    res.status(200).json({
-        success: true,
-        message: 'successful registration'
+        else{
+            res.status(200).json({
+                success: true,
+                message: 'successful registration'
+            })
+        }
     })
 });
 
 // Get all registered people
-app.get('/', (req, res) => {
+app.get('/all', (req, res) => {
     dbPool.query('SELECT * FROM stp.machathon_summit;', (error, results) => {
         if(error){
-            throw error;
+            res.status(500).json({
+                success: false,
+                message: 'internal server error'
+            });
+            console.log(error);
+            //throw error;
         }
-        res.status(200).json(results.rows);
+        else{
+            res.status(200).json(results.rows);
+        }
     })
 });
 
@@ -79,6 +86,20 @@ app.get('/cron', (req, res) => {
     res.status(200).json({
         state: "success"
     });
+})
+
+// an endpoint to check if the user already exists in the database
+app.get(`/`, async (req, res) => {
+    const email: string = decodeURI(req.query.email as string);
+    const nID = req.query.nid;
+    const qu = "SELECT * FROM stp.machathon_summit WHERE email=$1 OR national_id=$2;"
+    //
+    await dbPool.query(qu, [email, nID], (error, results) => {
+        if(error){
+            throw error;
+        }
+        res.status(200).json(results.rows);
+    })
 })
 
 
